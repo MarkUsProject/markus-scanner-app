80f8e8e35fe5e728b8d519c0f78d41de2c4bbb4a
Vijay <me@vijaybala.com>
2018-07-24 09:30:24 -0400

    Added Router

--- app/App.js
+++ app/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import MainScanner from './screens/MainScanner';
+import CourseInfoEntry from './screens/CourseInfoEntry';
+import {
+  Scene,
+  Router,
+  Stack,
+} from 'react-native-router-flux';
 
 export default class App extends Component {
   render () {
     return (
-      <MainScanner>
-      </MainScanner>
+      <Router>
+        <Stack key="root">
+          <Scene key="courseInfoEntry" component={CourseInfoEntry} hideNavBar="true"/>
+          <Scene key="mainScanner" component={MainScanner} hideNavBar="true"/>
+        </Stack>
+      </Router>
     );
   }
 }
---
+++ app/screens/CourseInfoEntry.js
@@ -0,0 +1,11 @@
+'use strict';
+import Scanner from '../components/Scanner';
+import React, { Component } from 'react';
+
+export default class CourseInfoEntry extends Component {
+  render() {
+    return (
+      <Scanner/>
+    )
+  }
+}
